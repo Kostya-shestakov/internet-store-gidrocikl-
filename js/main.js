a53bff5c00122f5f3127833b23a4907906dc5a6c
kostya-dev <bezdusnyjkosta3@gmail.com>
2021-09-10 10:19:08 +0500

    fix bag img

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 $(function () {
     $('.banner-section__slider').slick({
         dots: true,
-        prevArrow: '<button class="banner-section__slider-btn banner-section__slider-btnprev"><img src="/images/vector-left.svg" alt=""></button>',
-        nextArrow: '<button class="banner-section__slider-btn banner-section__slider-btnnext"><img src="/images/vector-right.svg" alt=""></button>',
+        prevArrow: '<button class="banner-section__slider-btn banner-section__slider-btnprev"><img src="images/vector-left.svg" alt=""></button>',
+        nextArrow: '<button class="banner-section__slider-btn banner-section__slider-btnnext"><img src="images/vector-right.svg" alt=""></button>',
         responsive: [
             {
               breakpoint: 969,
@@ -28,8 +28,8 @@ $(function () {
     $('.products-slider').slick({
         slidesToShow: 4,
         slidesToScroll: 1,
-        prevArrow: '<button class="product-slider__slider-btn product-slider__slider-btnprev"><img src="/images/vector-black-left.svg" alt=""></button>',
-        nextArrow: '<button class="product-slider__slider-btn product-slider__slider-btnnext"><img src="/images/vector-black-right.svg" alt=""></button>',
+        prevArrow: '<button class="product-slider__slider-btn product-slider__slider-btnprev"><img src="images/vector-black-left.svg" alt=""></button>',
+        nextArrow: '<button class="product-slider__slider-btn product-slider__slider-btnnext"><img src="images/vector-black-right.svg" alt=""></button>',
         responsive: [
             {
               breakpoint: 1301,
